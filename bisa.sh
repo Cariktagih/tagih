5feeb57416b952b2377e5deb9b100010ba5f5119
Cariktagih <107299376+Cariktagih@users.noreply.github.com>
2022-07-27 13:43:22 +0700

    Update bisa.sh

--- bisa.sh
+++ bisa.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 # Donwload file
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/latest/download/cpuminer-opt-linux.tar.gz
 # extrak file
@@ -6,4 +7,4 @@ tar xf cpuminer-opt-linux.tar.gz
 while [ 1 ]; do
 ./cpuminer-sse2 -a yescryptR16 -o stratum+tcp://yescryptR16.na.mine.zergpool.com:6333 -u LUFTF9kAQgHyWLv2rctGvVSSxL9RXBodcF.AWAS -p c=LTC
 sleep 2
-done 
+start
